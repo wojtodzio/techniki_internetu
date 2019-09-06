52467786216806727818da497b747a0feb15dbb0
Wojtek Wrona <wojtodzio@gmail.com>
2019-09-06 14:21:33 +0200

    Add seeds for interests

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        foreach (['Volleyball', 'Skiing', 'Computer Science', 'Being PW student', 'Finishing PW ASAP'] as $interest)
+        {
+            DB::table('interests')->insert([
+                'name' => $interest
+            ]);
+        }
     }
 }
