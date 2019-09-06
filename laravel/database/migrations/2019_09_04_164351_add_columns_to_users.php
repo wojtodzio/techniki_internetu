8673a9db271f059f6b147da578bbde8bcc2c3243
Wojtek Wrona <wojtodzio@gmail.com>
2019-09-06 11:59:00 +0200

    Add columns to user and Interest model

---
+++ laravel/database/migrations/2019_09_04_164351_add_columns_to_users.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname');
+            $table->string('login')->unique();
+            $table->string('country');
+            $table->string('city');
+            $table->string('street_address');
+            $table->enum('education', ['higher', 'middle', 'basic']);
+        });
+
+        Schema::create('user_interest', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('interest_id')->unsigned();
+            $table->foreign('interest_id')->references('id')->on('interests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_interest');
+        Schema::dropIfExists('interests');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname');
+            $table->dropColumn('login');
+            $table->dropColumn('country');
+            $table->dropColumn('city');
+            $table->dropColumn('street_address');
+            $table->dropColumn('education');
+        });
+    }
+}
